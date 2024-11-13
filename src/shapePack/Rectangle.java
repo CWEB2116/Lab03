f108893c33ca8066979f177821def821105442f1
Amelia Marrone <68938110+ameliamaedae@users.noreply.github.com>
2024-11-12 18:24:08 -0600

    homeworks

--- src/shapePack/Rectangle.java
+++ src/shapePack/Rectangle.java
@@ -4,4 +4,12 @@ public class Rectangle extends Shape {
     public void printRectangle() {
         System.out.println("This is Rectangle. Subclass of Shape");
     }
+
+    public void calculateArea(double length, double breadth) {
+        double area = length * breadth;
+        System.out.println("Area of Rectangle:");
+        System.out.println("Enter the measure of length: " + length);
+        System.out.println("Enter the measure of breadth: " + breadth);
+        System.out.println("RESULT: Area of Rectangle= " + area);
+    }
 }
--- src/shapeProcess/ShapeExecute.java
+++ src/shapeProcess/ShapeExecute.java
@@ -1,23 +1,41 @@
 package shapeProcess;
 
-import shapePack.Shape;
-import shapePack.Rectangle;
 import shapePack.Square;
-import shapePack.Circle;
 import shapePack.SemiCircle;
+import java.util.Scanner;
 
 public class ShapeExecute {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Create Square object and use it for both Square and Rectangle operations
         Square square = new Square();
         square.printShape();
         square.printRectangle();
         square.printSquare();
 
+        // Get side input for Square
+        System.out.println("\nArea of Square:");
+        System.out.print("Enter the measure of a side: ");
+        double side = scanner.nextDouble();
+        square.calculateArea(side);
+
+        // Get length and breadth input for Rectangle
+        System.out.println("\nArea of Rectangle:");
+        System.out.print("Enter the measure of length: ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter the measure of breadth: ");
+        double breadth = scanner.nextDouble();
+        square.calculateArea(length, breadth);
+
         System.out.println("----");
 
+        // Create SemiCircle object and call its methods
         SemiCircle semicircle = new SemiCircle();
         semicircle.printShape();
         semicircle.printSemiCircle();
         semicircle.printCircle();
+
+        scanner.close();
     }
 }
